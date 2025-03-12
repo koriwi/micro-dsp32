ea4061f68650b6a8cb5b04302889faca6357465a
Kilian Gosewisch <kilian2798@gmail.com>
2025-03-12 23:45:13 +0100

    adds mac based wifi name

--- firmware/main/wifi.c
+++ firmware/main/wifi.c
@@ -1,10 +1,12 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
+#include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 
-#define SSID "ESP32_AP"
-#define PASSWORD "12345678"
+#define SSID_BASE "micro-dsp32-"
+#define PASSWORD "microdsp32"
 
 void wifi_init_softap(void) {
   ESP_ERROR_CHECK(esp_netif_init());
@@ -14,18 +16,27 @@ void wifi_init_softap(void) {
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
+  uint8_t mac[6];
+  esp_wifi_get_mac(WIFI_IF_AP, mac);
+
+  char ssid[32];
+  snprintf(ssid, sizeof(ssid), "%s%02X%02X%02X", SSID_BASE, mac[3], mac[4],
+           mac[5]);
+
   wifi_config_t wifi_config = {
-      .ap = {.ssid = SSID,
+      .ap = {.ssid = "",
              .password = PASSWORD,
-             .ssid_len = strlen(SSID),
+             .ssid_len = 0,
              .channel = 1,
              .authmode = WIFI_AUTH_WPA_WPA2_PSK,
              .max_connection = 4},
   };
 
+  strcpy((char *)wifi_config.ap.ssid, ssid);
+
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
   ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
   ESP_ERROR_CHECK(esp_wifi_start());
 
-  ESP_LOGI("wifi", "Access Point \"%s\" started", SSID);
+  ESP_LOGI("wifi", "Access Point \"%s\" started", ssid);
 }
