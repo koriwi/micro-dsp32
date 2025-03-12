61127977e456e077f1ea4fc2a1368efc09396885
Kilian Gosewisch <kilian2798@gmail.com>
2025-03-12 14:39:23 +0100

    adds second channel ui

---
+++ webapp/src/chart.ts
@@ -0,0 +1,79 @@
+import type { ChartConfiguration, TooltipItem } from "chart.js";
+
+export const chartData = (dataChannel1?: any) => {
+  return {
+    data: {
+      datasets: [
+        {
+          data: dataChannel1 ?? [],
+          label: "Output 1",
+          fill: "start",
+          backgroundColor: "#ffffff55",
+          borderColor: "#ffffff",
+        },
+      ],
+    },
+    type: "line",
+    options: {
+      animation: {
+        duration: 250,
+      },
+      responsive: true,
+      plugins: {
+        legend: {
+          labels: {
+            color: "#fff", // Legend text color
+          },
+        },
+        tooltip: {
+          backgroundColor: "rgba(0, 0, 0, 0.8)", // Tooltip background
+          titleColor: "#fff", // Tooltip title color
+          bodyColor: "#fff", // Tooltip body color
+          callbacks: {
+            title: (item: TooltipItem<"line">[]) => (
+              console.log(item), item[0].parsed.x.toFixed(0) + " Hz"
+            ),
+            label: (item: TooltipItem<"line">) =>
+              item.parsed.y.toFixed(1) + " dB",
+          },
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: "#fff", // X-axis label color
+          },
+          type: "logarithmic",
+          min: 10,
+          title: { display: true, text: "Frequency (Hz)", color: "#fff" },
+          max: 20000,
+          grid: {
+            color: "rgba(255, 255, 255, 0.1)", // X-axis grid lines
+          },
+        },
+        y: {
+          ticks: {
+            color: "#fff", // X-axis label color
+          },
+          min: -30,
+          max: 10,
+          type: "linear",
+          axis: "y",
+          title: { display: true, text: "Gain (dB)", color: "#fff" },
+          grid: {
+            color: "rgba(255, 255, 255, 0.1)", // X-axis grid lines
+          },
+        },
+      },
+      elements: {
+        line: {
+          borderWidth: 2, // Line thickness
+        },
+        point: {
+          radius: 5, // Point radius
+          backgroundColor: "#fff", // Point color
+        },
+      },
+    },
+  } as ChartConfiguration;
+};
--- webapp/src/global.ts
+++ webapp/src/global.ts
@@ -1,7 +1,7 @@
 import { atom } from "nanostores";
 
 export interface PassConfig {
-  order: 1 | 2;
+  order: 0 | 1 | 2;
   q: number;
   freq: number;
 }
@@ -9,15 +9,29 @@ export interface EQConfig {
   lowpass: PassConfig;
   highpass: PassConfig;
 }
-export const eq = atom<EQConfig>({
-  lowpass: {
-    order: 1,
-    q: 0.7,
-    freq: 1000,
+export const eq = atom<EQConfig[]>([
+  {
+    lowpass: {
+      order: 1,
+      q: 0.7,
+      freq: 1000,
+    },
+    highpass: {
+      order: 1,
+      q: 0.7,
+      freq: 1000,
+    },
   },
-  highpass: {
-    order: 1,
-    q: 0.7,
-    freq: 1000,
+  {
+    lowpass: {
+      order: 1,
+      q: 0.7,
+      freq: 1000,
+    },
+    highpass: {
+      order: 1,
+      q: 0.7,
+      freq: 1000,
+    },
   },
-});
+]);
